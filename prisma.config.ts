2a972339749c461dfcc9ea2ea1664e0501bfe3c9
Samuel2005gp <sg2503818@gmail.com>
2026-05-06 16:39:55 -0500

    cambio de url para el front

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'prisma/config';
 export default defineConfig({
   datasource: {
     url: "postgresql://postgres:Highsoft123$$@db.dckxscmdtodkmwiufjgi.supabase.co:5432/postgres",
+    
   },
 });
