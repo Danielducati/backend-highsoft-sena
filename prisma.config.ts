2d6cd8d34923fc52b760f06b396a5020b3e80759
Samuel2005gp <sg2503818@gmail.com>
2026-05-06 19:34:41 -0500

    fix prisma v7 config

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
   datasource: {
-    url: "postgresql://postgres:Highsoft123$$@db.dckxscmdtodkmwiufjgi.supabase.co:5432/postgres",
-    
+    url: process.env.DATABASE_URL!,
   },
 });
